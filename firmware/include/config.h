57c2c834056f1520db4e33b32f484429a31184d3
MagnusKos <magnuskos@gmail.com>
2025-03-06 02:45:14 +0300

    Config additions

--- firmware/include/config.h
+++ firmware/include/config.h
@@ -11,12 +11,12 @@
 
 // Thermal sensing parameters
 #define SENSOR_ENGINE_BETA 3000
-#define SENSOR_ENGINE_R25 300   // In Kelvins
-#define SENSOR_ENGINE_RDIV 10   // In kOhms
+#define SENSOR_ENGINE_R25 50    // Nominal resistance at 25 Celicus, in kOhms
+#define SENSOR_ENGINE_RDIV 10   // Divider resistor's value kOhms
 
 #define SENSOR_AIR_BETA 3000
-#define SENSOR_AIR_R25 300      // In Kelvins
-#define SENSOR_AIR_RDIV 10      // In kOhms
+#define SENSOR_AIR_R25 10       // Nominal resistance at 25 Celicus, in kOhms
+#define SENSOR_AIR_RDIV 10      // Divider resistor's value kOhms
 
 // Engine temperatures: Ready-to-Go (RTG) and OverHeated (OH)
 #define ENGINE_RTG 75           // In Celcius
@@ -28,8 +28,9 @@
 #define BATT_ADD 0.0
 #define BATT_DIV 4.84            // Vbatmax / Vadcmax = DIV; 16V / 3.3V = 3.2
 
-// Connected i2c display's address, type, width, height
+// Connected i2c display's address, type, main font, width, height
 #define DISP_I2C_ADDR 0x3C
 #define DISP_TYPE U8X8_SH1106_128X32_VISIONOX_HW_I2C
+#define DISP_FONT u8x8_font_amstrad_cpc_extended_r
 #define DISP_W 128
 #define DISP_H 32
