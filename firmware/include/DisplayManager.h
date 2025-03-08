75699b59a180c67ffd338f3c7da0bace6e329d8e
MagnusKos <magnuskos@gmail.com>
2025-03-08 07:24:07 +0300

    DisplayManager New class, that creates and draws frames. It also provides methods of frame manipulation.

---
+++ firmware/include/DisplayManager.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <U8x8lib.h>
+
+#include "config.h"
+#include "pins.h"
+#include "Frame.h"
+#include "DataConnector.h"
+
+class DataConnector;
+
+enum MoveDir {
+    TOLEFT,
+    TORIGHT
+};
+
+class DisplayManager {
+    public:
+        DisplayManager(DISP_TYPE * const u8x8_ext, DataConnector * const dc);
+
+        void switchLFrame();
+        void switchRFrame();
+        void showGreeting() {};
+        void showWarning() {};
+
+        void draw();
+        void drawDirect(const char *s);
+    private:
+        DISP_TYPE * const u8x8_; // Needs inspection, i'd like to create this thing inside the class
+        DataConnector * const dc_;
+        uint8_t dispSizeX_, dispSizeY_;
+        Frame frameL_, frameR_, frameW_; // Three frames: Left, Right, Warning (invisible)
+
+        void drawFrame_(const Frame &frame);        // Puts frame's data on the display
+        void expandFrame_(Frame &frame);            // Expand the frame in x-direction
+        void shrinkFrame_(Frame &frame);            // Resize the frame in half in x-direction
+        void moveFrame_(Frame &frame, uint8_t dir); // Absolute positioning
+
+};
---
+++ firmware/src/DisplayManager.cpp
@@ -0,0 +1,97 @@
+#include "DisplayManager.h"
+
+DisplayManager::DisplayManager(DISP_TYPE * const u8x8_ext, DataConnector * const dc) :
+    u8x8_(u8x8_ext),
+    dc_{dc},
+    frameL_{dc},
+    frameR_{dc},
+    frameW_{dc}
+{ 
+    u8x8_->setFont(u8x8_font_amstrad_cpc_extended_r);
+    dispSizeX_ = u8x8_->getCols();
+    dispSizeY_ = u8x8_->getRows();
+
+    frameL_.setGeometry(
+        FrameGeom {
+            dispSizeX_, dispSizeY_, 0
+        }
+    );
+    frameL_.setContent(EMPTY);
+    frameR_.setContent(ENGINE);
+}
+
+void DisplayManager::drawFrame_(Frame const &frame) {
+    if (frame.getContentId() != EMPTY) {
+        FrameOutputData frData;
+        frData = frame.getFrameOutputData();
+
+        void (U8X8::*drawStringFunc)(uint8_t, uint8_t, const char *);
+        drawStringFunc = (frData.topLineScale == 1) ? &U8X8::drawString : &U8X8::draw1x2String; // Scale selection
+        (u8x8_->*drawStringFunc)(frData.topLinePosX, frData.topLinePosY, frData.topLineText.c_str()); // Write a string
+        drawStringFunc = (frData.botLineScale == 1) ? &U8X8::drawString : &U8X8::draw1x2String; // Scale selection
+        (u8x8_->*drawStringFunc)(frData.botLinePosX, frData.botLinePosY, frData.botLineText.c_str()); // Write a string
+
+    }
+}
+
+void DisplayManager::expandFrame_(Frame &frame) {
+    frame.setGeometry(
+        FrameGeom {
+            dispSizeX_, dispSizeY_, 0
+        }
+    );
+}
+
+void DisplayManager::shrinkFrame_(Frame &frame) {
+    frame.setGeometry(
+        FrameGeom {
+            static_cast<uint8_t>(dispSizeX_ / 2), dispSizeY_, 0
+        }
+    );
+}
+
+void DisplayManager::moveFrame_(Frame &frame, uint8_t dir) {
+    FrameGeom geomCur = frame.getFrameGeometry();
+    uint8_t hposNew = (dir == TORIGHT) ? dispSizeX_ / 2 - 1 : 0; // Too stupid, but hey, we have only two dirs in x-axis
+    frame.setGeometry(
+        FrameGeom {
+            geomCur.width, geomCur.height, hposNew
+        }
+    );
+}
+
+void DisplayManager::draw() {
+    u8x8_->clearDisplay();
+    drawFrame_(frameL_);
+    drawFrame_(frameR_);
+    u8x8_->refreshDisplay();
+ }
+
+void DisplayManager::drawDirect(const char *s) {
+    u8x8_->clearDisplay();
+    u8x8_->draw1x2String(1, 1, s);
+ }
+
+void DisplayManager::switchLFrame() {
+    frameL_.switchContent();
+    if (frameR_.isEmpty())
+        expandFrame_(frameL_);
+    else {
+        shrinkFrame_(frameL_);
+        moveFrame_(frameL_, TOLEFT);
+    }
+    if (frameL_.isEmpty())      // A gentleman's rule, in case of emptying the frame
+        expandFrame_(frameR_);
+}
+
+void DisplayManager::switchRFrame() {
+    frameR_.switchContent();
+    if (frameL_.isEmpty())
+        expandFrame_(frameR_);
+    else {
+        shrinkFrame_(frameR_);
+        moveFrame_(frameR_, TORIGHT);
+    }
+    if (frameR_.isEmpty())      // A gentleman's rule, in case of emptying the frame
+        expandFrame_(frameL_);
+ }
