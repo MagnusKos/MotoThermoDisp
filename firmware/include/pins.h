f8c1c9b66a5bf13a155c1e066b7dfb201f9f166e
MagnusKos <magnuskos@gmail.com>
2025-03-21 04:59:08 +0300

    Configs update Some improvements to config.h made. Added new parameters, changed already existed. Beautified it (kinda). Some changes to pins.h. Most of the config is made for MapleMini. Small adjustments to platformio.ini: experimenting with debugging on ESP32S3.

--- firmware/include/pins.h
+++ firmware/include/pins.h
@@ -1,15 +1,30 @@
 #pragma once
 
 
-/* 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
     Section: pins setup
     Edit these parameters first to fulfil your setup. 
-*/
+* * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#define ADC_ENGINE_PIN 11
-#define ADC_AIR_PIN 10
-#define ADC_BATT_PIN 9
 
+/* * * * *
+ ADC pins.
+ Used for sensors.
+* * * * */
+#define ADC_ENGINE_PIN PA0
+#define ADC_AIR_PIN PA1
+#define ADC_BATT_PIN PA2
+
+// #define ADC_ENGINE_PIN 11
+// #define ADC_AIR_PIN 10  //PA1 on STM32F103
+// #define ADC_BATT_PIN 9  //PA2 on STM32F103
+
+
+/* * * * *
+ Digital pins.
+ Used for input/output.
+ Mostly self-explanatory
+* * * * */
 #define I2C_SDA_PIN 15
 #define I2C_SDC_PIN 16
 
