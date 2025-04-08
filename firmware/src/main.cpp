1c22fd895f15ccc42621886f775a671cfaa07367
MagnusKos <magnuskos@gmail.com>
2025-04-08 04:04:45 +0300

    Sensors' initialization Small adjustments to the sensors' initialization section in the setup() function.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -21,16 +21,18 @@ DISP_TYPE *u8x8Disp;
 
 
 void setup() {
-
   u8x8Disp = new DISP_TYPE(U8X8_PIN_NONE, I2C_SDC_PIN, I2C_SDA_PIN);
   u8x8Disp->begin();
 
-  sensAir = new Sensor(
-    SensorLimits{AIR_TC, AIR_OH, AIR_OK}, ADC_AIR_PIN);
-  sensEngine = new Sensor(
-    SensorLimits{ENGINE_TC, ENGINE_OH, ENGINE_RTG}, ADC_ENGINE_PIN);
-  sensBatt = new Sensor(
-    SensorLimits{BATT_UV, BATT_OV, BATT_OK}, ADC_BATT_PIN); 
+  sensAir = new SensorTemperature(
+    SensorLimits{AIR_TC, AIR_OH, AIR_OK}, ADC_AIR_PIN,
+    NTCParams {SENSOR_AIR_BETA, SENSOR_AIR_R25, SENSOR_AIR_RDIV});
+  sensEngine = new SensorTemperature(
+    SensorLimits{ENGINE_TC, ENGINE_OH, ENGINE_RTG}, ADC_ENGINE_PIN,
+    NTCParams {SENSOR_ENGINE_BETA, SENSOR_ENGINE_R25, SENSOR_ENGINE_RDIV});
+  sensBatt = new SensorVoltage(
+    SensorLimits{BATT_UV, BATT_OV, BATT_OK}, ADC_BATT_PIN,
+    PowerParams {BATT_ADD, BATT_DIV}); 
 
   butLeft = new Button2(BUT_LEFT_PIN, INPUT);
   butRight = new Button2(BUT_RIGHT_PIN, INPUT);
